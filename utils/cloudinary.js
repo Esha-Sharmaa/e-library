5af9939ca568273e674887a02d0f7a5be6d5f364
Esha Sharma <eshasharmakb22@gmail.com>
2024-06-06 17:04:44 +0530

    add cloudinary setup

---
+++ utils/cloudinary.js
@@ -0,0 +1,22 @@
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        const response = cloudinary.uploader(localFilePath, { resource_type: "auto" });
+        console.log("file uploaded successfully on cloudinary");
+        fs.unlinkSync(localFilePath);
+        return response.url;
+        
+    } catch (error) {
+        console.error(" Error while uploading file on cloudinary", error);
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
